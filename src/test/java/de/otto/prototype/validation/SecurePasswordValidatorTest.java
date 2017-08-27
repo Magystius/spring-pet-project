37084518021b37a48b4a11dc486cb1d80bb4d4ae
Tim Dekarz <tim.dekarz@gmx.net>
2017-08-27 11:15:22 +0200

    introduce junit5 parameterized tests

--- src/test/java/de/otto/prototype/validation/SecurePasswordValidatorTest.java
+++ src/test/java/de/otto/prototype/validation/SecurePasswordValidatorTest.java
@@ -1,54 +1,49 @@
 package de.otto.prototype.validation;
 
-import com.tngtech.java.junit.dataprovider.DataProvider;
-import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.hibernate.validator.internal.util.annotationfactory.AnnotationDescriptor;
 import org.hibernate.validator.internal.util.annotationfactory.AnnotationFactory;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@RunWith(DataProviderRunner.class)
-public class SecurePasswordValidatorTest {
+class SecurePasswordValidatorTest {
 
-    private SecurePasswordValidator testee;
+	private SecurePasswordValidator testee;
 
-    @DataProvider
-    public static Object[][] invalidPasswordProvider() {
-        return new Object[][]{
-                {"111"},
-                {"11111111111111111111111111111111"},
-                {"invalid"},
-                {""},
-                {null},
-        };
-    }
+	@BeforeEach
+	void setup() {
+		testee = new SecurePasswordValidator();
+		testee.initialize(implementAnnotationInterface("[0-9]+"));
+	}
 
-    @Before
-    public void setup() {
-        testee = new SecurePasswordValidator();
-        testee.initialize(implementAnnotationInterface("[0-9]+"));
-    }
+	private SecurePassword implementAnnotationInterface(String pattern) {
+		AnnotationDescriptor<SecurePassword> descriptor = new AnnotationDescriptor<>(SecurePassword.class);
+		descriptor.setValue("pattern", pattern);
+		return AnnotationFactory.create(descriptor);
+	}
 
-    private SecurePassword implementAnnotationInterface(String pattern) {
-        AnnotationDescriptor<SecurePassword> descriptor = new AnnotationDescriptor<>(SecurePassword.class);
-        descriptor.setValue("pattern", pattern);
-        return AnnotationFactory.create(descriptor);
-    }
+	@ParameterizedTest
+	@ValueSource(strings = {"111", "11111111111111111111111111111111", "invalid", ""})
+	@DisplayName("should return false for every unsecure password")
+	void shouldValidateInsecurePasswords(String insecurePassword) {
+		assertThat(testee.isValid(insecurePassword, null), is(false));
+	}
 
-    @Test
-    @UseDataProvider("invalidPasswordProvider")
-    public void shouldValidateUnsecurePasswords(String unsecurePassword) {
-        assertThat(testee.isValid(unsecurePassword, null), is(false));
-    }
+	@Test
+	@DisplayName("should return false for null password")
+	void shouldValidateNullPassword() {
+		assertThat(testee.isValid(null, null), is(false));
+	}
 
-    @Test
-    public void shouldValidateSecurePassword() {
-        testee.initialize(implementAnnotationInterface(".*"));
-        assertThat(testee.isValid("securePassword", null), is(true));
-    }
+	@Test
+	@DisplayName("should return true for a secure password")
+	void shouldValidateSecurePassword() {
+		testee.initialize(implementAnnotationInterface(".*"));
+		assertThat(testee.isValid("securePassword", null), is(true));
+	}
 }
