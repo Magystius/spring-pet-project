d7cadb45542fce38f5911354f6e689d608c208d8
Tim Dekarz <tim.dekarz@otto.de>
2017-08-31 17:36:16 +0200

    replace hibernate proxy with mocks for validator unit test

--- src/test/java/de/otto/prototype/validation/SecurePasswordValidatorTest.java
+++ src/test/java/de/otto/prototype/validation/SecurePasswordValidatorTest.java
@@ -1,49 +1,51 @@
 package de.otto.prototype.validation;
 
-import org.hibernate.validator.internal.util.annotationfactory.AnnotationDescriptor;
-import org.hibernate.validator.internal.util.annotationfactory.AnnotationFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 class SecurePasswordValidatorTest {
 
-	private SecurePasswordValidator testee;
+    @Mock
+    private SecurePassword securePassword;
 
-	@BeforeEach
-	void setup() {
-		testee = new SecurePasswordValidator();
-		testee.initialize(implementAnnotationInterface("[0-9]+"));
-	}
+    private SecurePasswordValidator testee;
 
-	private SecurePassword implementAnnotationInterface(String pattern) {
-		AnnotationDescriptor<SecurePassword> descriptor = new AnnotationDescriptor<>(SecurePassword.class);
-		descriptor.setValue("pattern", pattern);
-		return AnnotationFactory.create(descriptor);
-	}
+    @BeforeEach
+    void setup() {
+        initMocks(this);
+        final String pattern = "[0-9]+";
+        given(securePassword.pattern()).willReturn(pattern);
+        testee = new SecurePasswordValidator();
+        testee.initialize(securePassword);
+    }
 
-	@ParameterizedTest(name = "{index} -> {0}")
-	@ValueSource(strings = {"111", "11111111111111111111111111111111", "invalid", ""})
-	@DisplayName("should return false when unsecure password is")
-	void shouldValidateInsecurePasswords(String insecurePassword) {
-		assertThat(testee.isValid(insecurePassword, null), is(false));
-	}
+    @ParameterizedTest(name = "{index} -> {0}")
+    @ValueSource(strings = {"111", "11111111111111111111111111111111", "invalid", ""})
+    @DisplayName("should return false when unsecure password is")
+    void shouldValidateInsecurePasswords(String insecurePassword) {
+        assertThat(testee.isValid(insecurePassword, null), is(false));
+    }
 
-	@Test
-	@DisplayName("should return false for null password")
-	void shouldValidateNullPassword() {
-		assertThat(testee.isValid(null, null), is(false));
-	}
+    @Test
+    @DisplayName("should return false for null password")
+    void shouldValidateNullPassword() {
+        assertThat(testee.isValid(null, null), is(false));
+    }
 
-	@Test
-	@DisplayName("should return true for a secure password")
-	void shouldValidateSecurePassword() {
-		testee.initialize(implementAnnotationInterface(".*"));
-		assertThat(testee.isValid("securePassword", null), is(true));
-	}
+    @Test
+    @DisplayName("should return true for a secure password")
+    void shouldValidateSecurePassword() {
+        given(securePassword.pattern()).willReturn(".*");
+        testee.initialize(securePassword);
+        assertThat(testee.isValid("securePassword", null), is(true));
+    }
 }
